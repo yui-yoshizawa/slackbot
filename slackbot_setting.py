9bab1f32b84ff347fac88d9fb4becb11fb6dcba5
yui-yoshizawa <yui.yossy.0114@gmail.com>
2019-11-29 16:33:55 +0900

    firstcommit

---
+++ slackbot_setting.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+# botアカウントのトークンを指定
+API_TOKEN = os.environ.get("testtest")
+
+# このbot宛のメッセージで、どの応答にも当てはまらない場合の応答文字列
+DEFAULT_REPLY = "何言ってんだこいつ"
+
+# プラグインスクリプトを置いてあるサブディレクトリ名のリスト
+PLUGINS = ['plugins']
